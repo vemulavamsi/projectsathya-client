7e185104c280d5d06fd2618fdf7acdde44e5fa11
Vamsi_vemula <vamsi.v@pycube.com>
2023-12-11 03:36:08 -0500

    modified

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://localhost:3000/';
+// const AUTH_API = 'http://localhost:3000/';
 
+const AUTH_API = 'http://172.31.88.20:3000/';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
